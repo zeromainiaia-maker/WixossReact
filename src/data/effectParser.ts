0816a8ba26fc5d34cbc5fd7c8b69679e4dd8d786
karkadoll1011-crypto <296094326+karkadoll1011-crypto@users.noreply.github.com>
2026-07-08 06:01:17 +0900

    auto: src/data/effectParser.ts
    M	src/data/effectParser.ts

--- src/data/effectParser.ts
+++ src/data/effectParser.ts
@@ -29,7 +29,7 @@ import type {
   PowerModifyAction,
 } from '../types/effects';
 import {
-  parseNum, parseLevelFilter, parseColorFilter, parseStoryFilter, parseGuardFilter, parseEnergyCosts, toHalf, stripRuleParens, parseSuperlative,
+  parseNum, parseLevelFilter, parseColorFilter, parseStoryFilter, parseGuardFilter, parseEnergyCosts, toHalf, stripRuleParens, parseSuperlative, parseSelfComparison,
 } from './parserUtils';
 import { parseSentencePart1 } from './parsers/parseSentencePart1';
 import { parseSentencePart2 } from './parsers/parseSentencePart2';
