934a414efb31ceb90d185cb141ae1b7503e714b4
karkadoll1011-crypto <296094326+karkadoll1011-crypto@users.noreply.github.com>
2026-08-15 16:54:04 +0900

    auto: src/screens/BattleScreen.tsx
    M	src/screens/BattleScreen.tsx

--- src/screens/BattleScreen.tsx
+++ src/screens/BattleScreen.tsx
@@ -117,7 +117,7 @@ import { resolveSigniZonePlacement, activateNextTurnSigniZoneBlocks } from './ba
 import { clearUntilOppTurnEffects } from './battle/untilOppTurn';
 import { attackFieldTrashCost, canPayAttackFieldTrashCost, clearAttackFieldTrashCosts, deterministicAttackFieldTrashZones, payAttackFieldTrashCost } from './battle/attackFieldTrashCost';
 import { canSigniAttack } from './battle/signiAttackGate';
-import { signiAttackBanHandDiscardCost, signiAttackBanColorlessCost } from './battle/signiAttackBan';
+import { signiAttackBanHandDiscardCost, signiAttackBanColorlessCost, lrigAttackBanCost } from './battle/signiAttackBan';
 import { assistLrigAttackableSlots, lrigSlotTop, markLrigSlotDown, type LrigAttackSlot } from './battle/assistLrigAttack';
 import { signiCannotDealDamageToOpponent } from './battle/signiDamageGate';
 import { sideAttackEmptyZoneDealsDamage } from './battle/sideAttackDamage';
