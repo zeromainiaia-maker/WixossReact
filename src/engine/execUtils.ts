c4fbf5a307a33a075d7b83767c734a8c6e3cadff
karkadoll1011-crypto <296094326+karkadoll1011-crypto@users.noreply.github.com>
2026-08-16 23:55:46 +0900

    auto: src/engine/execUtils.ts
    M	src/engine/execUtils.ts

--- src/engine/execUtils.ts
+++ src/engine/execUtils.ts
@@ -254,7 +254,7 @@ export function resolveOptionalCostSpec(a: StubAction, ctx: ExecCtx): OptionalCo
     fieldToLrigTrash: a.fieldToLrigTrash, trashOwnKey: a.trashOwnKey, fieldDown: a.fieldDown, lrigDown: a.lrigDown, down_self: a.down_self, selfToEnergy: a.selfToEnergy,
     beat_signi: a.beat_signi, beat_signi_from_trash: a.beat_signi_from_trash,
     life_crash: a.life_crash, lifeTrash: a.lifeTrash, lifeToHand: a.lifeToHand,
-    deckTrash: a.deckTrash, charmTrash: a.charmTrash,
+    deckTrash: a.deckTrash, charmTrash: a.charmTrash, exceed: a.exceed,
     trashArtsFromLrigDeck: a.trashArtsFromLrigDeck, removeOppVirus: a.removeOppVirus,
     levelUnavailable: perLevel && level <= 0,
   };
